f6bf1043c2d2f8e663e10be9b6bab483cf456740
Akshay Sankle <32200419+zukros@users.noreply.github.com>
2021-03-12 20:17:36 +0530

    Update Image.py

--- Image.py
+++ Image.py
@@ -1,11 +1,14 @@
 import cv2
+#import
 
 img = cv2.imread("Thor.png")
+#mention path of the Image with extensions
 
 cv2.imshow("Output", img)
+#name of the window
 cv2.waitKey(1)
 
 
 # waitKey(seconds)
-# waitKey(0) infinite, captured image will not be terminated until user ends it 
-# waitkey(1) 1ms,captured imnage will be terminated after desired sec. 
+# waitKey(0) infinite, window will not be terminated until user ends it 
+# waitkey(1) 1ms, window will be terminated after desired sec. 
